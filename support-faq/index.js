21198d79298225f300a487ba7d7afc104d145c81
Rayen Magpantay <raye.magp@gmail.com>
2016-08-11 19:40:22 +1000

    Added postcode function

--- support-faq/index.js
+++ support-faq/index.js
@@ -4,15 +4,37 @@ var Alexa = require('alexa-app');
 //create new Alexa application
 var skill = new Alexa.app('post');
 
-var reprompt = 'Tell me what kind of package you want to send.';
 
 //onLaunch
 skill.launch(function(request, response) {
-    var prompt = 'Tell me what kind of package you want to send.';
-    response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    if (response.session('postcode')){
+        var prompt = 'Tell me what kind of package you want to send.';
+        response.say(prompt).reprompt(prompt).shouldEndSession(false);
+    }
+    else
+    {
+        var prompt = 'Tell me what your postcode is.';
+        response.say(prompt).reprompt(prompt).shouldEndSession(false);
+    }
+    
 });
 
-//onIntent
+//method for storing postcode
+skill.intent('setPostCodeIntent', 
+    //pass slots and utterances first
+    {
+        'slots': {
+        'SET_POST_CODE': 'AMAZON.FOUR_DIGIT_NUMBER'
+        },
+        'utterances': ['{|save}{|postcode|post code}{SET_POST_CODE}{postcode|post code}']
+    },
+    function(req,res){
+        var postcode = req.slots('SET_POST_CODE');
+        res.session('postcode',postcode).say(postcode + " is saved.").shouldEndSession(false);
+    }
+);
+
+//method for calculating postage
 skill.intent('SupportIntent', {
         'slots': {
         'POST_TYPE': 'POST_TYPE'
@@ -24,6 +46,16 @@ skill.intent('SupportIntent', {
             res.say('forty dollars');
         if(req.slot('POST_TYPE') == 'local')
             res.say('twenty dollars');
+    }
+);
+
+//method for getting postcode - called last in the schema!
+skill.intent('getPostCodeIntent',
+    function(req , res){
+            if(res.session('postcode'))
+                res.say(postcode.toString()).shouldEndSession(false);
+            else
+                res.say("I don't have your postcode. Tell me what your postcode is.").shouldEndSession(false);
     }
 );
 
